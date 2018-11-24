3d9329cc19c37217348ee5c5e27e12a542a68b2f
손승범 <32606420+asdfghjkkl11@users.noreply.github.com>
2018-11-24 17:46:14 +0900

    Add files via upload

--- web programming/sovigation/models.py
+++ web programming/sovigation/models.py
@@ -39,4 +39,11 @@ class FoodC(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
+class Board(models.Model):
+    subject = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=50, blank=True)
+    created_date = models.DateField(null=True, blank=True)
+    mail = models.CharField(max_length=50, blank=True)
+    memo = models.CharField(max_length=200, blank=True)
+    hits = models.IntegerField(null=True, blank=True)
 # Create your models here.
--- web programming/sovigation/views.py
+++ web programming/sovigation/views.py
@@ -5,6 +5,8 @@ from sovigation.models import Lib
 from sovigation.models import FoodA
 from sovigation.models import FoodB
 from sovigation.models import FoodC
+from sovigation.models import Board
+from sovigation.pagingHelper import pagingHelper
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseRedirect
 rowsPerPage = 5
@@ -31,3 +33,133 @@ def foodc(request):
 
 def home(request):
     return render_to_response('home.html', {})
+
+
+def board(request):
+  boardList = Board.objects.order_by('-id')[0:2]
+  current_page = 1
+  totalCnt = Board.objects.all().count()
+  pagingHelperIns = pagingHelper()
+  totalPageList = pagingHelperIns.getTotalPageList(totalCnt, rowsPerPage)
+  return render_to_response('BoardPage.html', {'boardList': boardList, 'totalCnt': totalCnt,
+                                                      'current_page': current_page, 'totalPageList': totalPageList})
+
+
+def write(request):
+  return render_to_response('writeBoard.html')
+
+
+@csrf_exempt
+def dowrite(request):
+  br = Board(subject=request.POST['subject'],
+             name=request.POST['name'],
+             mail=request.POST['email'],
+             memo=request.POST['memo'],
+             created_date=timezone.now(),
+             hits=0
+             )
+  br.save()
+
+  url = '/board?current_page=1'
+  return HttpResponseRedirect(url)
+
+
+def listpage(request):
+  current_page = request.GET['current_page']
+  totalCnt = Board.objects.all().count()
+
+  boardList = Board.objects.raw('SELECT Z.* FROM(SELECT X.*, ceil( @rownum / %s ) as page FROM ( SELECT ID,SUBJECT,NAME, CREATED_DATE, MAIL,MEMO,HITS \
+                                FROM sovigation_board  ORDER BY ID DESC ) X ) Z WHERE page = %s',
+                                [rowsPerPage, current_page])
+  print 'boardList=', boardList, 'count()=', totalCnt
+  pagingHelperIns = pagingHelper()
+  totalPageList = pagingHelperIns.getTotalPageList(totalCnt, rowsPerPage)
+
+  return render_to_response('BoardPage.html', {'boardList': boardList, 'totalCnt': totalCnt,
+                                               'current_page': int(current_page),
+                                               'totalPageList': totalPageList})
+
+
+def viewwork(request):
+
+  pk = request.GET['memo_id']
+  boardData = Board.objects.get(id=pk)
+
+  Board.objects.filter(id=pk).update(hits=boardData.hits + 1)
+
+  return render_to_response('viewMemo.html', {'memo_id': request.GET['memo_id'],
+                                              'current_page': request.GET['current_page'],
+                                              'searchStr': request.GET['searchStr'],
+                                              'boardData': boardData})
+
+
+def searchedpage(request):
+  searchStr = request.GET['searchStr']
+  pageForView = request.GET['pageForView']
+
+  totalCnt = Board.objects.filter(subject__contains=searchStr).count()
+
+  pagingHelperIns = pagingHelper()
+  totalPageList = pagingHelperIns.getTotalPageList(totalCnt, rowsPerPage)
+
+  boardList = Board.objects.raw("""SELECT Z.* FROM ( SELECT X.*, ceil(@rownum / %s) as page \
+                                FROM ( SELECT ID,SUBJECT,NAME, CREATED_DATE, MAIL,MEMO,HITS FROM sovigation_board \
+                                WHERE SUBJECT LIKE '%%'||%s||'%%' ORDER BY ID DESC) X ) Z WHERE page = %s""",
+                                [rowsPerPage, searchStr, pageForView])
+
+  return render_to_response('SearchedPage.html', {'boardList': boardList, 'totalCnt': totalCnt,
+                                                  'pageForView': int(pageForView), 'searchStr': searchStr,
+                                                  'totalPageList': totalPageList})
+
+
+def listupdate(request):
+  memo_id = request.GET['memo_id']
+  current_page = request.GET['current_page']
+  searchStr = request.GET['searchStr']
+  boardData = Board.objects.get(id=memo_id)
+  return render_to_response('listUpdate.html', {'memo_id': request.GET['memo_id'],
+                                                   'current_page': request.GET['current_page'],
+                                                   'searchStr': request.GET['searchStr'],
+                                                   'boardData': boardData})
+
+
+@csrf_exempt
+def updateboard(request):
+    memo_id = request.POST['memo_id']
+    current_page = request.POST['current_page']
+    searchStr = request.POST['searchStr']
+    Board.objects.filter(id=memo_id).update(
+                                            mail=request.POST['mail'],
+                                            subject=request.POST['subject'],
+                                            memo=request.POST['memo']
+                                            )
+    url = '/board?current_page=' + str(current_page)
+    return HttpResponseRedirect(url)
+
+
+def delete(request):
+  memo_id = request.GET['memo_id']
+  current_page = request.GET['current_page']
+
+  p = Board.objects.get(id=memo_id)
+  p.delete()
+  totalCnt = Board.objects.all().count()
+  pagingHelperIns = pagingHelper()
+  totalPageList = pagingHelperIns.getTotalPageList(totalCnt, rowsPerPage)
+
+  if int(current_page) in totalPageList:
+    current_page = current_page
+  else:
+    current_page = int(current_page) - 1
+
+  url = '/board?current_page=' + str(current_page)
+  return HttpResponseRedirect(url)
+
+
+@csrf_exempt
+def searchWithSubject(request):
+    searchStr = request.POST['searchStr']
+    print 'searchStr', searchStr
+
+    url = '/searchedPage?searchStr=' + searchStr +'&pageForView=1'
+    return HttpResponseRedirect(url)
