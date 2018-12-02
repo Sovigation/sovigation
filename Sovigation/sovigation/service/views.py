467c45ce314949fd34bb4ca90cd7e0c369c502de
손승범 <32606420+asdfghjkkl11@users.noreply.github.com>
2018-12-02 22:02:37 +0900

    Update views.py

--- Sovigation/sovigation/service/views.py
+++ Sovigation/sovigation/service/views.py
@@ -1,7 +1,46 @@
 # Create your views here.
 from django.shortcuts import render
-from django.shortcuts import render
+
+
+def login(request):
+  return render('service/login.html', {})
+
+
+def about(request):
+  return render('service/about.html', {})
+
+
+def assignment(request):
+  return render('service/assignment.html', {})
+
+
+def board(request):
+  return render('service/board.html', {})
+
+
+def contact(request):
+  return render('contact.html', {})
+
+
+def food(request):
+  return render('food.html', {})
+
 
 def index(request):
-    return render(request, 'service/index.html', {})
+  return render('index.html', {})
 
+
+def grade(request):
+  return render('grade.html', {})
+
+
+def lib(request):
+  return render('lib.html', {})
+
+
+def myservice(request):
+  return render('myService.html', {})
+
+
+def todo(request):
+  return render('to_do.html', {})
