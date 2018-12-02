e890dedb47e4a3600a6e8131968d7ed3dac7b8ac
genie97 <qndnd1@naver.com>
2018-12-03 01:25:36 +0900

    connect all templates

--- Sovigation/sovigation/sovigation/urls.py
+++ Sovigation/sovigation/sovigation/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('service.urls')),
-
+    path(r'^admin/', admin.site.urls),
+    path('', include('service.urls')),
 ]
