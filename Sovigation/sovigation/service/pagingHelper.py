1086d10e312e8ef046ace37d8eeeedab97f15363
손승범 <32606420+asdfghjkkl11@users.noreply.github.com>
2018-12-11 21:22:33 +0900

    Update pagingHelper.py

--- Sovigation/sovigation/service/pagingHelper.py
+++ Sovigation/sovigation/service/pagingHelper.py
@@ -3,12 +3,10 @@ class pagingHelper:
     def getTotalPageList(self, total_cnt, rowsPerPage):
         if (total_cnt % rowsPerPage) == 0:
             self.total_pages = total_cnt / rowsPerPage
-            print
-            "getTotalPage #1"
+            print 'getTotalPage #1'
         else:
             self.total_pages = (total_cnt / rowsPerPage) + 1
-            print
-            "getTotalPage #2"
+            print 'getTotalPage #2'
 
         self.totalPageList = []
         for j in range(self.total_pages):
@@ -19,3 +17,16 @@ class pagingHelper:
     def __init__(self):
         self.total_pages = 0
         self.totalPageList = 0
+
+    def getCurrentPageList(self, BoardList, rowsPerPage, current_page):
+        current_page = int(current_page)
+        index = 0
+        for i in range(0, current_page):
+            index += i * rowsPerPage
+        PageList = []
+        for i in range(index, len(BoardList)):
+            PageList.append(BoardList[i])
+            if len(PageList) == rowsPerPage:
+                break
+
+        return PageList
