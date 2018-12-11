9f0c13f63ca292eadf67d95de3f7f25b1a29ec1a
손승범 <32606420+asdfghjkkl11@users.noreply.github.com>
2018-12-11 21:28:37 +0900

    Update views.py

--- Sovigation/sovigation/service/views.py
+++ Sovigation/sovigation/service/views.py
@@ -1,55 +1,53 @@
-# -*- coding: utf-8 -*-
+import logging
+
+logger = logging.getLogger(__name__)
+from django.shortcuts import render
 from django.shortcuts import render_to_response
-from django.utils import timezone
-from sovigation.models import LoginRequest
-from sovigation.models import LoginResult
-from sovigation.models import Lib
-from sovigation.models import FoodA
-from sovigation.models import FoodB
-from sovigation.models import FoodC
-from sovigation.models import Board
-from sovigation.pagingHelper import pagingHelper
-from django.views.decorators.csrf import csrf_exempt
-from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseRedirect
+from .models import Lib
+from .models import FoodA
+from .models import FoodB
+from .models import FoodC
+from .models import UserInfo
+from .models import LoginRequest
+from .models import LoginResult
+from .models import Grade
+from .forms import GradeForm
+from .pagingHelper import pagingHelper
+from django.core.exceptions import ObjectDoesNotExist
+from django.views.decorators.csrf import csrf_exempt
 import time
+
 rowsPerPage = 5
 
 
-#효진
-def login(request):
-  return render_to_response('service/login.html', {})
+def index(request):
+    return render(request, 'service/index.html')
 
 
 def about(request):
-  return render_to_response('service/about.html', {})
+    return render(request, 'service/about.html')
 
 
 def assignment(request):
-  return render_to_response('service/assignment.html', {})
+    return render(request, 'service/assignment.html')
 
 
 def board(request):
-  return render_to_response('service/board.html', {})
-
-
-def contact(request):
-  return render_to_response('service/contact.html', {})
+    return render(request, 'service/board.html')
 
 
 def food(request):
     boardListA = FoodA.objects.order_by('-id')[0:6]
     boardListB = FoodB.objects.order_by('-id')[0:6]
     boardListC = FoodC.objects.order_by('-id')[0:6]
-    return render_to_response('service/food.html', {'boardListA': boardListA, 'boardListB': boardListB, 'boardListC': boardListC})
-
-
-def index(request):
-  return render_to_response('service/index.html', {})
+    return render_to_response('service/food.html',
+                              {'boardListA': boardListA, 'boardListB': boardListB, 'boardListC': boardListC})
 
 
 def grade(request):
-  return render_to_response('service/grade.html', {})
+    form = GradeForm()
+    return render(request, 'service/grade.html', {'form': form})
 
 
 def lib(request):
@@ -57,14 +55,51 @@ def lib(request):
     return render_to_response('service/lib.html', {'boardList': boardList})
 
 
-def myservice(request):
-  return render_to_response('service/myService.html', {})
-
-def todo(request):
-  return render_to_response('service/to_do.html', {})
+def login(request):
+    return render(request, 'service/login.html')
 
 
-#승범
+def myService(request):
+    return render(request, 'service/myService.html')
+
+
+def to_do(request):
+    return render(request, 'service/to_do.html')
+
+
+def authenticate(username, password):
+    try:
+        user = UserInfo.objects.get(username=username, password=password)
+    except ObjectDoesNotExist:
+        user = None
+    return user
+
+
+def insert_new_user(username, password):
+    user = UserInfo(username=username, password=password)
+    user.save()
+
+
+# def insert_grade(request):
+#     # if request.method == 'GET':
+#     userId = request.session['user']
+#     user = UserInfo.objects.get(username=userId)
+#     subject = request.GET['_subject']
+#     grade = request.GET['_grade']
+#     credit = request.GET['_credit']
+#     semester = request.GET['_semester']
+#     if request.GET['_major'] is 'true':
+#         major = True
+#     else:
+#         major = False
+#
+#     record = Grade(
+#         user=user, grade=grade, credit=credit, semester=semester, major=major, subject=subject
+#     )
+#     record.save()
+#     return render(request, 'servlce/lib.html')
+# else:
+# return render(request, 'servlce/food.html')
 
 @csrf_exempt
 def dologin(request):
